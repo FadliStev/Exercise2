61af86ce927b5d14626b4aed4daf003ec60e1b5d
FadliStev <wiranto.akbar.ft21@mail.umy.ac.id>
2023-06-22 04:18:13 +0700

    Add project files.

---
+++ Exercise2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'sINAR_BAKTIDataSet.Transaksi' table. You can move, or remove it, as needed.
+            this.transaksiTableAdapter.Fill(this.sINAR_BAKTIDataSet.Transaksi);
+
+        }
+    }
+}
